22c6270639169b895915c292f5c9d3a2e10bff8a
Xaver Fischer <Simerax@github.com>
2020-05-09 22:26:57 +0200

    more basic stack tests

--- test/flux_stack_tests.c
+++ test/flux_stack_tests.c
@@ -5,6 +5,7 @@
 START_TEST (test_stack_push_pop)
 {
     flux_stack* stack = flux_stack_init();
+    ck_assert_int_eq(flux_stack_get_error(stack), flux_stack_error_no_error);
 
     flux_stack_ipush(stack, 23);
     flux_stack_pop(stack);
@@ -41,10 +42,26 @@ START_TEST (test_stack_underflow)
 }
 END_TEST
 
+START_TEST (test_stack_overflow)
+{
+    flux_stack* stack = flux_stack_init();
+
+    for(int i = 0; i <= FLUX_STACK_SIZE; i++)
+        flux_stack_ipush(stack, 10);
+
+    ck_assert_int_eq(flux_stack_get_error(stack), flux_stack_error_overflow);
+
+
+    flux_stack_free(stack);
+
+}
+END_TEST
+
 
 TEST_HELPER_START(flux_stack);
 
 TEST_HELPER_ADD_TEST(test_stack_push_pop);
 TEST_HELPER_ADD_TEST(test_stack_underflow);
+TEST_HELPER_ADD_TEST(test_stack_overflow);
 
 TEST_HELPER_END_TEST
