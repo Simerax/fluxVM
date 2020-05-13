4292bb1f79604028e2a0d1c81625e3062330ae2e
Xaver Fischer <Simerax@github.com>
2020-05-13 21:26:30 +0200

    read file and execute it

--- bin/main.c
+++ bin/main.c
@@ -1,47 +1,37 @@
 #include<stdio.h>
+#include<errno.h>
+#include<stdlib.h>
 #include"flux_vm.h"
 
 int main(int argc, char** argv)
 {
+    if(argc < 2) {
+        puts("No Arguments specified. You need to specify at least a .fvm file");
+        return 1;
+    }
+
+    char* filename = argv[argc - 1];
+
+    FILE* f = fopen(filename, "r");
+    if(f == NULL) {
+        printf("Failed to open file %s Error: %d", filename, errno);
+        return 1;
+    }
+
+    fseek(f, 0, SEEK_END);
+    long f_length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char* fvm_filecontent = malloc(f_length);
+    fread(fvm_filecontent, 1, f_length, f);
+    fclose(f);
+
+
+    FluxCode* code = flux_code_init(fvm_filecontent, f_length);
     FluxVM* vm = flux_vm_init();
 
-    /*store value 5 in variable 0*/
-    flux_vm_ipush(vm, 5);
-    flux_vm_ipush(vm, 0);
-    flux_vm_store(vm);
-
-    /*put variable 0 onto the stack*/
-    flux_vm_load(vm, 0);
-
-    /*put integer 10 onto the stack*/
-    flux_vm_ipush(vm, 10);
-
-    /*integer addition of the two stack elements*/
-    flux_vm_iadd(vm);
-
-    /*store the result of add in variable at index 1*/
-    flux_vm_ipush(vm, 1);
-    flux_vm_store(vm);
-
-
-    /*load variable from index 1 and print it*/
-    flux_vm_load(vm, 1);
-    flux_vm_print(vm);
-
-
-    /* load variable 1 convert it to double*/
-    flux_vm_load(vm, 1);
-    flux_vm_itod(vm);
-
-    /* store new value in local var 1 */
-    flux_vm_ipush(vm, 1);
-    flux_vm_store(vm);
-
-    flux_vm_load(vm, 1);
-    flux_vm_print(vm);
-
-
-
+    flux_vm_execute(vm, code);
+    flux_code_free(code);
     flux_vm_free(vm);
     return 0;
 }
