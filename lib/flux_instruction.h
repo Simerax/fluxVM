905e267854e4bd4d942e23def5e3f9c7e2e9a3f4
Xaver Fischer <Simerax@github.com>
2020-05-30 18:58:31 +0200

    basic documentation of the different instructions

--- lib/flux_instruction.h
+++ lib/flux_instruction.h
@@ -2,24 +2,103 @@
 #define FLUX_INSTRUCTION_H
 
 typedef enum {
+
+    // Add the two integers which are on top of the stack together and put the result back on the stack
     IADD = 1,
+
+    // Convert the Integer on the stack to a double
     ITOD,
+
+    // Push an integer to the stack
+    // Params:
+    //  int number
+    //
+    //  char bytecode[] = {
+    //      IPUSH,
+    //      0,0,0,10 
+    //  };
     IPUSH,
+
+    // Pop a value from the stack
     POP,
+
+    // Load a variable from index onto the stack
+    // Params:
+    //  int index
+    //
+    //  // load variable from index 1
+    //  char bytecode[] = {
+    //      LOAD,
+    //      0,0,0,1
+    //  };
     LOAD,
+
+    // Opposite of LOAD.
+    // Store Variable at given index and remove it from the stack
+    // Params:
+    //  int index
+    //
+    //  // Store value at index 3
+    //  char bytecode[] = {
+    //      STORE,
+    //      0,0,0,3
+    //  };
     STORE,
+
+    // PRINT whatever is on the stack
     PRINT,
+
+    // JumpSubRoutine - jumps uncoditionally to given address and puts the return address on the stack
+    // Params:
+    //  Int ReturnAddress
+    //
+    //  char bytecode[] = {
+    //      JSR,
+    //      0,0,0,20
+    //  };
     JSR,
+
+    // Compare the top two elements of the stack.
+    // removes the elements from the stack
+    // Sets cmp_flag which is used by the various conditional Jump instructions
     CMP,
+
+    // Jump to given address if cmp_flag is set to GREATER
     JG,
+
+    // Jump to given address if cmp_flag is set to EQUAL
     JE,
+    // Jump to given address if cmp_flag is set to LESS
     JL,
+
+    // Jump to given address if cmp_flag is set to GREATER or EQUAL
     JGE,
+    // Jump to given address if cmp_flag is set to LESS or EQUAL
     JLE,
+
+    // Inspect a given variable - right now its like print
     INSPECT,
+
+    // subtract two integers from each other and put the result on the stack
     ISUB,
+
+    // multiply two integers and put the result on the stack
     IMUL,
+
+    // divide two integers and put the result on the stack
     IDIV,
+
+    // push a String on the stack
+    // Params:
+    //  int str_len
+    //  char[] string
+    //
+    //  char bytecode[] = {
+    //      SPUSH,
+    //      0,0,0,6
+    //      'H','e','l','l','o','\0'
+    //  };
+    //
     SPUSH,
 
     // Defines a new Exception
